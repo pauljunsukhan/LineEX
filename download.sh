96fd465985e63628dd9415c58436163148a73f39
Paul Han <io.paul@kato.cx>
2025-02-21 17:32:12 +0000

    fixed download script weights are still on authors data is on paul han drive

--- download.sh
+++ download.sh
@@ -1,68 +1,86 @@
-echo "Downloading weights"
-gdown 176BjH_6W-HRoU9RvsysSNW27GVIHPlHV
-mkdir temp/
-mkdir data/
-mkdir modules/CE_detection/ckpts
-mkdir modules/KP_detection/ckpts
-mkdir modules/Grouping_legend_mapping/ckpts
-echo "Extracting checkpoints"
-unzip -d temp/ weights.zip
-mv temp/weights/checkpoint110.pth modules/CE_detection/ckpts/checkpoint110.pth
-mv temp/weights/ckpt_30.t7 modules/Grouping_legend_mapping/ckpts/ckpt_30.t7
-mv temp/weights/mlp_ckpt.t7 modules/Grouping_legend_mapping/ckpts/mlp_ckpt.t7
-mv temp/weights/ckpt_L.t7 modules/KP_detection/ckpts/ckpt_L.t7
-mv temp/weights/ckpt_L+D.t7 modules/KP_detection/ckpts/ckpt_L+D.t7
-echo "Moved checkpoints to corresponding dir"
+#!/bin/bash
 
+# Install wget if not present
+if ! command -v wget &> /dev/null; then
+    echo "Installing wget..."
+    apt-get update && apt-get install -y wget
+fi
 
- while getopts "T:V:L:" flag
-    do
-             case "${flag}" in
-                    T) TRAIN=${OPTARG};;
-                    V) VAL=${OPTARG};;
-                    L) TEST=${OPTARG};;
-                    # I) TEST_PROJECTID=${OPTARG};;
-             esac
-    done
-    echo "TRAIN: $TEST_USER";
-    echo "VAL: $TEST_PWD";
-    echo "TEST: $TEST_JOBID";
+echo "Creating directories..."
+mkdir -p temp/
+mkdir -p data/
+mkdir -p modules/CE_detection/ckpts
+mkdir -p modules/KP_detection/ckpts
+mkdir -p modules/Grouping_legend_mapping/ckpts
 
-if [ $TRAIN == "True"] || [ $VAL == "True" ] || [ $TEST == "True" ]
+# Function to download from Google Drive
+download_from_gdrive() {
+    FILE_ID=$1
+    DESTINATION=$2
+    CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$FILE_ID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$FILE_ID" -O $DESTINATION
+    rm -rf /tmp/cookies.txt
+}
+
+echo "Downloading model weights..."
+echo "This might take a while..."
+
+# Download model weights
+echo "Downloading checkpoint110.pth..."
+download_from_gdrive "1z6nLrIfjPRghJkLzPIWBLs5KPPaxgnfx" "modules/CE_detection/ckpts/checkpoint110.pth"
+
+echo "Downloading ckpt_30.t7..."
+download_from_gdrive "1N49INpPKXWbAk7DCk8dRJX9MUnUa184d" "modules/Grouping_legend_mapping/ckpts/ckpt_30.t7"
+
+echo "Downloading mlp_ckpt.t7..."
+download_from_gdrive "1FtA-gJoU6Nf1nUTkMX2g_909l1rn96N0" "modules/Grouping_legend_mapping/ckpts/mlp_ckpt.t7"
+
+echo "Downloading ckpt_L.t7..."
+download_from_gdrive "1LFSCjf1gnK346iZQRCUPL_yLPmfP2Zyl" "modules/KP_detection/ckpts/ckpt_L.t7"
+
+echo "Downloading ckpt_L+D.t7..."
+download_from_gdrive "1QyPykDBusW9LW0YejuTJfgfPr4qICtq8" "modules/KP_detection/ckpts/ckpt_L+D.t7"
+
+while getopts "T:V:L:" flag
+do
+    case "${flag}" in
+        T) TRAIN=${OPTARG};;
+        V) VAL=${OPTARG};;
+        L) TEST=${OPTARG};;
+    esac
+done
+
+if [ "$TRAIN" = "True" ] || [ "$VAL" = "True" ] || [ "$TEST" = "True" ]
 then 
-   mkdir data/
+   mkdir -p data/
+   echo "Downloading data files..."
 fi
 
-if [ $TRAIN == "True" ]
+if [ "$TRAIN" = "True" ]
 then
-   echo "Downloading Train Data"
-   gdown 1x-2A0TXTY2cL7390NSvVXrt9skIuUKkR
-   mkdir data/train
-   unzip -d temp/ train.zip
-   mv temp/train/anno data/train/
-   mv temp/train/images data/train/
-
-
+   echo "Downloading training data..."
+   mkdir -p data/train
+   download_from_gdrive "1-2A0TXTY2cL7390NSvVXrt9skIuUKkR" "temp/train.zip"
+   unzip temp/train.zip -d data/train/
 fi
 
-if [ $VAL == "True" ]
+if [ "$VAL" = "True" ]
 then
-   echo "Downloading Val Data"
-   gdown 1wr1pezZ3teMiS3k4TCNcNvH7TigtiGMF
-   mkdir data/val
-   unzip -d temp/ val.zip
-   mv temp/val/anno data/val/
-   mv temp/val/images data/val/
+   echo "Downloading validation data..."
+   mkdir -p data/val
+   download_from_gdrive "1wr1pezZ3teMiS3k4TCNcNvH7TigtiGMF" "temp/val.zip"
+   unzip temp/val.zip -d data/val/
 fi
 
-if [ $TEST == "True" ]
+if [ "$TEST" = "True" ]
 then
-   echo "Downloading Test Data"
-   gdown 1nDSGrYqUrwBg-8YkcvWLmGU5mtyfA1uz
-   mkdir data/test
-   unzip -d temp/ test.zip
-   mv temp/test/anno data/test/
-   mv temp/test/images data/test/
+   echo "Downloading test data..."
+   mkdir -p data/test
+   download_from_gdrive "1nDSGrYqUrwBg-8YkcvWLmGU5mtyfA1uz" "temp/test.zip"
+   unzip temp/test.zip -d data/test/
 fi
-    
+
+echo "Cleaning up..."
 rm -rf temp/
+
+echo "Download complete!"
